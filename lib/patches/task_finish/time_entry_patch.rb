0c166e88d4360b9ab469f4ce3733a93c49470a78
IkegzI <kegz@mail.ru>
2020-01-29 15:50:04 +0800

    fix bugs

--- lib/patches/task_finish/time_entry_patch.rb
+++ lib/patches/task_finish/time_entry_patch.rb
@@ -14,7 +14,7 @@ module TaskFinish
       module InstanceMethods
         #add columns run_time and total spend hours
         def validate_time_entry_ext
-          errors.add :issue_id, :task_is_finish if issue.status_id == 3 and CustomImprovements.load_settings[:improvements_disable_finish] == 0
+          errors.add :issue_id, :is_finish if issue.status_id == 3 and CustomImprovements.load_settings[:improvements_disable_finish] == 0
           errors.add :hours, :invalid if hours && (hours < 0 || hours >= 1000)
           errors.add :project_id, :invalid if project.nil?
           errors.add :issue_id, :invalid if (issue_id && !issue) || (issue && project!=issue.project) || @invalid_issue_id
