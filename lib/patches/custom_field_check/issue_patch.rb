48435ca543d29975ebfeeb94b7d81483caaa5e61
IkegzI <kegz@mail.ru>
2020-01-30 16:40:16 +0800

    fix bugs

--- lib/patches/custom_field_check/issue_patch.rb
+++ lib/patches/custom_field_check/issue_patch.rb
@@ -9,6 +9,7 @@ module CustomFieldCheck
         base.send(:include, InstanceMethods)
         base.class_eval do
           alias_method :validate_custom_field_values, :validate_custom_field_values_ext
+          alias_method :validate_issue, :validate_issue_ext
         end
       end
 
@@ -20,7 +21,7 @@ module CustomFieldCheck
         def validate_custom_field_values_ext
 
           def check_error_field(arg, setting)
-            if CustomImprovements.load_settings[setting] == 0
+            if CustomImprovements.load_settings[setting] == 0 && arg.value == '1'
               if arg.custom_field.id == CustomImprovements.load_settings[:improvements_disable_id_custom_fields_check]
                 return true if estimated_hours.nil? or estimated_hours.to_i <= 0
               end
@@ -31,15 +32,74 @@ module CustomFieldCheck
           user = new_record? ? author : current_journal.try(:user)
           a = editable_custom_field_values(user).each(&:validate_value)
           if new_record? || custom_field_values_changed?
-
             if CustomImprovements.load_settings[:improvements_disable_custom_fields_check] == 0
               a.each do |item|
                 errors.add :base, :error_estimate if check_error_field(item, :improvements_disable_custom_fields_check)
               end
             end
+
             editable_custom_field_values(user).each(&:validate_value)
           end
         end
+
+
+        def validate_issue_ext
+
+          if due_date && start_date && (start_date_changed? || due_date_changed?) && due_date < start_date
+            errors.add :due_date, :greater_than_start_date
+          end
+
+          if start_date && start_date_changed? && soonest_start && start_date < soonest_start
+            errors.add :start_date, :earlier_than_minimum_start_date, :date => format_date(soonest_start)
+          end
+
+          if fixed_version
+            if !assignable_versions.include?(fixed_version)
+              errors.add :fixed_version_id, :inclusion
+            elsif reopening? && fixed_version.closed?
+              errors.add :base, I18n.t(:error_can_not_reopen_issue_on_closed_version)
+            end
+          end
+
+          # Checks that the issue can not be added/moved to a disabled tracker
+          if project && (tracker_id_changed? || project_id_changed?)
+            if tracker && !project.trackers.include?(tracker)
+              errors.add :tracker_id, :inclusion
+            end
+          end
+
+          if assigned_to_id_changed? && assigned_to_id.present?
+            unless assignable_users.include?(assigned_to)
+              errors.add :assigned_to_id, :invalid
+            end
+          end
+
+          # Checks parent issue assignment
+          if @invalid_parent_issue_id.present?
+            errors.add :parent_issue_id, :invalid
+          elsif @parent_issue
+            if !valid_parent_project?(@parent_issue)
+              errors.add :parent_issue_id, :invalid
+            elsif (@parent_issue != parent) && (
+            self.would_reschedule?(@parent_issue) ||
+                @parent_issue.self_and_ancestors.any? {|a| a.relations_from.any? {|r| r.relation_type == IssueRelation::TYPE_PRECEDES && r.issue_to.would_reschedule?(self)}}
+            )
+              errors.add :parent_issue_id, :invalid
+            elsif !closed? && @parent_issue.closed?
+              # cannot attach an open issue to a closed parent
+              errors.add :base, :open_issue_with_closed_parent
+            elsif !new_record?
+              # moving an existing issue
+              if move_possible?(@parent_issue)
+                # move accepted
+              else
+                errors.add :parent_issue_id, :invalid
+              end
+            end
+          end
+        end
+
+
 
       end
     end
