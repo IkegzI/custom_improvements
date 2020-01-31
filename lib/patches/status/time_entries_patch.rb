3954e8c6417eff1788a7bd60a55ae7cb66d3a0f3
IkegzI <kegz@mail.ru>
2020-01-31 13:46:09 +0800

    fix bugs

--- lib/patches/status/time_entries_patch.rb
+++ lib/patches/status/time_entries_patch.rb
@@ -22,49 +22,52 @@ module Status
             return
           end
 
-          call_hook(:controller_timelog_edit_before_save, { :params => params, :time_entry => @time_entry })
+          call_hook(:controller_timelog_edit_before_save, {:params => params, :time_entry => @time_entry})
 
           if @time_entry.save
-            if @issue.status_id == 1 and Setting.plugin_custom_improvements['improvements_disable_status'] == '0'
-              @issue.status_id = 2
-              @issue.save
+            binding.pry
+            if Setting.plugin_custom_improvements['improvements_disable_status'] == '0'
+              if (@issue.status_id == IssueStatus.find_by(name: 'Новая').id)
+                @issue.status_id = IssueStatus.find_by(name: 'В работе').id
+                @issue.save
+              end
             end
 
 
-            respond_to do |format|
-              format.html {
-                flash[:notice] = l(:notice_successful_create)
-                if params[:continue]
-                  options = {
-                      :time_entry => {
-                          :project_id => params[:time_entry][:project_id],
-                          :issue_id => @time_entry.issue_id,
-                          :activity_id => @time_entry.activity_id
-                      },
-                      :back_url => params[:back_url]
-                  }
-                  if params[:project_id] && @time_entry.project
-                    redirect_to new_project_time_entry_path(@time_entry.project, options)
-                  elsif params[:issue_id] && @time_entry.issue
-                    redirect_to new_issue_time_entry_path(@time_entry.issue, options)
-                  else
-                    redirect_to new_time_entry_path(options)
-                  end
+          respond_to do |format|
+            format.html {
+              flash[:notice] = l(:notice_successful_create)
+              if params[:continue]
+                options = {
+                    :time_entry => {
+                        :project_id => params[:time_entry][:project_id],
+                        :issue_id => @time_entry.issue_id,
+                        :activity_id => @time_entry.activity_id
+                    },
+                    :back_url => params[:back_url]
+                }
+                if params[:project_id] && @time_entry.project
+                  redirect_to new_project_time_entry_path(@time_entry.project, options)
+                elsif params[:issue_id] && @time_entry.issue
+                  redirect_to new_issue_time_entry_path(@time_entry.issue, options)
                 else
-                  redirect_back_or_default project_time_entries_path(@time_entry.project)
+                  redirect_to new_time_entry_path(options)
                 end
-              }
-              format.api  { render :action => 'show', :status => :created, :location => time_entry_url(@time_entry) }
-            end
-          else
-
-            respond_to do |format|
-              format.html { render :action => 'new' }
-              format.api  { render_validation_errors(@time_entry) }
-            end
+              else
+                redirect_back_or_default project_time_entries_path(@time_entry.project)
+              end
+            }
+            format.api { render :action => 'show', :status => :created, :location => time_entry_url(@time_entry) }
           end
-         end
+        else
+
+          respond_to do |format|
+            format.html { render :action => 'new' }
+            format.api { render_validation_errors(@time_entry) }
+          end
+        end
       end
     end
   end
+end
 end
