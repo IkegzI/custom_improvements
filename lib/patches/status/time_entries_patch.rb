91d42a1c8963b156645be6f1baf135d7cc6d28b6
IkegzI <kegz@mail.ru>
2020-02-04 14:18:12 +0800

    fix bugs

--- lib/patches/status/time_entries_patch.rb
+++ lib/patches/status/time_entries_patch.rb
@@ -23,7 +23,6 @@ module Status
           end
 
           call_hook(:controller_timelog_edit_before_save, {:params => params, :time_entry => @time_entry})
-
           if @time_entry.save
             if Setting.plugin_custom_improvements['improvements_disable_status'] == '0'
               if @issue.status_id == 1
