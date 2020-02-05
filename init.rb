8d1b2cb981e1eda0bfff9366bfde8a2e7b4a252f
IkegzI <kegz@mail.ru>
2020-02-05 18:01:27 +0800

    fix bugs050220/13

--- init.rb
+++ init.rb
@@ -1,4 +1,4 @@
-# require_dependency 'custom_improvements'
+require_dependency 'custom_improvements'
 Redmine::Plugin.register :custom_improvements do
   name 'SunStrike Redmine Custom Improvements'
   author 'Pecherskyi Alexei'
@@ -11,7 +11,7 @@ Redmine::Plugin.register :custom_improvements do
   ON_OFF_CONST = [['Включен', 0], ['Выключен', 1]]
   # improvements_disable_on_tracker: 0,
   #     improvements_disable_id_tracker: 0, improvements_disable_project_add_task: 0
-  # default_settings = CustomImprovements.load_settings
+  default_settings = CustomImprovements.load_settings
 
   # settings default: {params: default_settings}, partial: 'improvements/settings/custom_improvements'
   settings default: {'improvements_disable_overrun' => '1','improvements_disable_status' => '1', 'improvements_disable_finish' => '1', 'improvements_disable_date' => '0', 'improvements_disable_on_tracker' => '0', 'improvements_disable_id_tracker' => '0'}, partial: 'improvements/settings/custom_improvements'
@@ -24,16 +24,16 @@ Redmine::Plugin.register :custom_improvements do
   tf = 'task_finish'
   path = './lib/patches'
   object_to_prepare.to_prepare do
-    # require_relative "#{path}/#{tor}/query_patch.rb"
-    # require_relative "#{path}/#{tor}/queries_helper_patch.rb"
-    # require_relative "#{path}/#{st}/time_entries_patch.rb"
-    # require_relative "#{path}/#{st}/issues_controller_patch.rb"
-    # require_relative "#{path}/#{tf}/time_entry_patch.rb"
-    # Query.send(:include, TimeTaskOverrun::Patches::QueryPatch)
-    # QueriesHelper.send(:include, TimeTaskOverrun::Patches::QueriesHelperPatch)
-    # TimelogController.send(:include, Status::Patches::TimelogControllerPatch)
-    # IssuesController.send(:include, Status::Patches::IssuesControllerPatch)
-    # TimeEntry.send(:include, TaskFinish::Patches::TimeEntryPatch)
+    require_relative "#{path}/#{tor}/query_patch.rb"
+    require_relative "#{path}/#{tor}/queries_helper_patch.rb"
+    require_relative "#{path}/#{st}/time_entries_patch.rb"
+    require_relative "#{path}/#{st}/issues_controller_patch.rb"
+    require_relative "#{path}/#{tf}/time_entry_patch.rb"
+    Query.send(:include, TimeTaskOverrun::Patches::QueryPatch)
+    QueriesHelper.send(:include, TimeTaskOverrun::Patches::QueriesHelperPatch)
+    TimelogController.send(:include, Status::Patches::TimelogControllerPatch)
+    IssuesController.send(:include, Status::Patches::IssuesControllerPatch)
+    TimeEntry.send(:include, TaskFinish::Patches::TimeEntryPatch)
   end
 
 end
