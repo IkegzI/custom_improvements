f8c41cc4bcba97aa5e2cdabe3f4dc136f37fbc7a
IkegzI <kegz@mail.ru>
2020-02-05 17:44:38 +0800

    fix bugs050220/11

--- lib/custom_improvements.rb
+++ lib/custom_improvements.rb
@@ -14,31 +14,31 @@ module CustomImprovements
   # end
 
 
-  class << self
-    
-    def load_settings(plugin_id = 'custom_improvements')
-      cached_settings_name = '@load_settings_' + plugin_id
-      cached_settings = instance_variable_get(cached_settings_name)
-      if cached_settings.nil?
-        data = YAML.safe_load(ERB.new(IO.read(Rails.root.join('plugins',
-                                                              plugin_id,
-                                                              'config',
-                                                              'settings.yml'))).result) || {}
-        instance_variable_set(cached_settings_name, data.symbolize_keys)
-      else
-        cached_settings
-      end
-    end
-
-    def options_select_custom_fields
-      CustomField.all.map { |item| [item.name, item.id] }
-    end
-
-    def options_select_tracker
-      Tracker.all.map { |item| [item.name, item.id] }
-    end
-
-  end
-  
+  # class << self
+  #
+  #   def load_settings(plugin_id = 'custom_improvements')
+  #     cached_settings_name = '@load_settings_' + plugin_id
+  #     cached_settings = instance_variable_get(cached_settings_name)
+  #     if cached_settings.nil?
+  #       data = YAML.safe_load(ERB.new(IO.read(Rails.root.join('plugins',
+  #                                                             plugin_id,
+  #                                                             'config',
+  #                                                             'settings.yml'))).result) || {}
+  #       instance_variable_set(cached_settings_name, data.symbolize_keys)
+  #     else
+  #       cached_settings
+  #     end
+  #   end
+  #
+  #   def options_select_custom_fields
+  #     CustomField.all.map { |item| [item.name, item.id] }
+  #   end
+  #
+  #   def options_select_tracker
+  #     Tracker.all.map { |item| [item.name, item.id] }
+  #   end
+  #
+  # end
+  #
 end
 
