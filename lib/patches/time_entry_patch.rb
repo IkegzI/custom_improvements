0a85279f70edaea093dec1ad3d8fb2049a8077b1
IkegzI <kegz@mail.ru>
2020-02-13 16:29:31 +0800

    fix bugs 13022020

--- lib/patches/time_entry_patch.rb
+++ lib/patches/time_entry_patch.rb
@@ -6,64 +6,58 @@ module CustomImprovements
       include CustomImprovements
 
       def self.included(base)
-        base.send(:include, InstanceMethods)
         base.class_eval do
+          unloadable
+
           validate :validate_time_entry_ext, on: [:create, :update]
         end
       end
 
-      module InstanceMethods
-        #add columns run_time and total spend hours
-        def validate_time_entry_ext
+      #add columns run_time and total spend hours
+      def validate_time_entry_ext
 
-          def errors_add_spent_on?(arg, setting)
-            if Setting.plugin_custom_improvements[setting] == '0'
-              i = 0
-              date = arg
-              date_now = Date.strptime(Time.now.strftime('%Y-%m-%d'), '%Y-%m-%d')
-              return true if date > date_now
-            end
-            false
+        def errors_add_spent_on?(arg, setting)
+          if Setting.plugin_custom_improvements[setting] == '0'
+            i = 0
+            date = arg
+            date_now = Date.strptime(Time.now.strftime('%Y-%m-%d'), '%Y-%m-%d')
+            return true if date > date_now
           end
-
-          def errors_add_issue_is_fihish?(arg, setting)
-            if Setting.plugin_custom_improvements[setting] == '0'
-              begin
-                if arg.status_id == 3
-                  return true
-                end
-              rescue
-                errors.add :base
-              end
-            end
-            false
-          end
-
-          def errors_add_issue_on_tracker?(hours, arg, setting)
-            if Setting.plugin_custom_improvements[setting] == '0'
-              if arg.tracker_id == Setting.plugin_custom_improvements['improvements_disable_id_tracker'].to_i
-                return true if arg.estimated_hours.to_i < arg.spent_hours.to_i + hours.to_i
-              end
-            end
-            false
-          end
-
-          errors.add :issue_id, :is_finish if errors_add_issue_is_fihish?(issue, 'improvements_disable_finish')
-          errors.add :spent_on, :date_arrived if errors_add_spent_on?(spent_on, 'improvements_disable_date')
+          false
         end
+
+        def errors_add_issue_is_fihish?(arg, setting)
+          if Setting.plugin_custom_improvements[setting] == '0'
+            begin
+              if arg.status_id == 3
+                return true
+              end
+            rescue
+              errors.add :base
+            end
+          end
+          false
+        end
+
+        def errors_add_issue_on_tracker?(hours, arg, setting)
+          if Setting.plugin_custom_improvements[setting] == '0'
+            if arg.tracker_id == Setting.plugin_custom_improvements['improvements_disable_id_tracker'].to_i
+              return true if arg.estimated_hours.to_i < arg.spent_hours.to_i + hours.to_i
+            end
+          end
+          false
+        end
+
+        binding.pry
+        if hours.to_f > 0 and issue.status_id == 1
+          issue.status_id = 2
+        end
+
+        errors.add :issue_id, :is_finish if errors_add_issue_is_fihish?(issue, 'improvements_disable_finish')
+        errors.add :spent_on, :date_arrived if errors_add_spent_on?(spent_on, 'improvements_disable_date')
       end
     end
   end
 end
 
-##on_time_entry > 0.00566666666666669 for print error
-# def errors_add_on_time_entry(hours, setting)
-#           #   if Setting.plugin_custom_improvements[setting] == '0'
-#           #     if hours < 0.00566666666666669
-#           #       return true
-#           #     end
-#           #   end
-#           #   false
-#           # end
-#
-#           # errors.add :base, :on_time_entry if errors_add_on_time_entry(hours, 'improvements_disable_overrun')
+     # errors.add :base, :on_time_entry if errors_add_on_time_entry(hours, 'improvements_disable_overrun')
