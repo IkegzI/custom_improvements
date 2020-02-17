84837e671ffad9804d5c54e13984c00e06af00ea
IkegzI <kegz@mail.ru>
2020-02-17 13:39:25 +0800

    170220/1

--- app/helpers/task_off_helper.rb
+++ app/helpers/task_off_helper.rb
@@ -14,47 +14,56 @@ module TaskOffHelper
   end
 
   def custom_check_box_project_create
-    if Setting.plugin_custom_improvements['improvements_disable_project_add_task'] == '0'
-      unless CustomField.where(type: 'ProjectCustomField').find_by(name: 'Запрещать создание тикетов')
-        cf = CustomField.create(
-            type: "ProjectCustomField",
-            name: "Запрещать создание тикетов",
-            field_format: "bool",
-            possible_values: nil,
-            regexp: "",
-            min_length: nil,
-            max_length: nil,
-            is_required: true,
-            is_for_all: false,
-            is_filter: false,
-            position: 1,
-            searchable: false,
-            default_value: "1",
-            editable: true,
-            visible: true,
-            multiple: false,
-            format_store: {
-                "url_pattern" => "",
-                "edit_tag_style" => ""
-            },
-            description: "")
-        # Setting.plugin_custom_improvements['improvements_field_id_taboo_task'] = a.id.to_s
-      else
-        cf = CustomField.find_by(name: 'Запрещать создание тикетов')
-        # Setting.plugin_custom_improvements['improvements_field_id_taboo_task'] = cf.id
-        cf.update(is_required: true, visible: true)
-      end
-    elsif Setting.plugin_custom_improvements['improvements_disable_project_add_task'] == '1'
-      begin
-        cf = CustomField.find_by(name: 'Запрещать создание тикетов')
-        cf.update(is_required: false, visible: false)
-          # Setting.plugin_custom_improvements['improvements_field_id_taboo_task'] = ''
-      rescue
-        puts 'Error: Field is not access!'
-      end
+    unless CustomField.where(type: 'ProjectCustomField').find_by(name: 'Запрещать создание тикетов')
+      cf = CustomField.create(
+          type: "ProjectCustomField",
+          name: "Запрещать создание тикетов",
+          field_format: "bool",
+          possible_values: nil,
+          regexp: "",
+          min_length: nil,
+          max_length: nil,
+          is_required: true,
+          is_for_all: false,
+          is_filter: false,
+          position: 1,
+          searchable: false,
+          default_value: "1",
+          editable: true,
+          visible: true,
+          multiple: false,
+          format_store: {
+              "url_pattern" => "",
+              "edit_tag_style" => ""
+          },
+          description: "")
+      # Setting.plugin_custom_improvements['improvements_field_id_taboo_task'] = a.id.to_s
     end
-    cf.id
+
+    cf_id = Setting.plugin_custom_improvements['improvements_field_id_taboo_task'].to_i
+    if cf_id > 0
+      cf = CustomField.find(cf_id)
+    else
+      cf = cf || CustomField.find_by(name: 'Запрещать создание тикетов')
+    end
+
+    begin
+
+      if Setting.plugin_custom_improvements['improvements_disable_project_add_task'] == '1'
+        cf.update(is_required: false, visible: false)
+      end
+
+      if Setting.plugin_custom_improvements['improvements_disable_project_add_task'] == '0'
+        cf.update(is_required: true, visible: true)
+
+      end
+
+    rescue
+      puts 'Error: Field is not access!'
+    end
+    cf.id || 0
   end
+
 
   def check_project_level
     check = false
