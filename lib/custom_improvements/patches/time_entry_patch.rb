32705d1be5c9344f9200e42a94a30ad20de07118
IkegzI <kegz@mail.ru>
2020-02-27 22:02:21 +0800

    fix bugs

--- lib/custom_improvements/patches/time_entry_patch.rb
+++ lib/custom_improvements/patches/time_entry_patch.rb
@@ -55,7 +55,7 @@ module CustomImprovements
               field_value = item.value
             end
           end
-          if arg.status_id < 5 and field_value == "1"
+          if arg.status_id < 5 and field_value != "0"
             if Setting.plugin_custom_improvements[setting] == '0'
               if TrackerCheck.where(tracker_id: arg.tracker_id).size > 0
                 check = true if arg.estimated_hours.to_f.round(2) < arg.spent_hours.to_f.round(2) + hours.to_f.round(2)
